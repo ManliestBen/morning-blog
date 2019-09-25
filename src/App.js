b9a011d12db0e8cbc8b6a7698d3c0e60767d9585
ManliestBen <benjamintmanley@gmail.com>
2019-09-25 10:32:19 -0500

    add title/content/user

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ import Footer from './Footer'
 class App extends Component {
   
   state = {
-    isShowing : true
+    isShowing : true,
+    posts : [
+      {
+        title : "My first confetti blog post.",
+        content : "I love confetti!!!  It is the best!!!",
+        user : "Ben"
+      },
+      {
+        title: "Pandas are fun!",
+        content:  "They are fun, but not as fun as confetti.",
+        user: "CWill833"
+      }
+    ]
+
   }
    
   handleClick = event => {
@@ -15,11 +28,23 @@ class App extends Component {
     })
   }
   render() {
+    
     const title = <h1>Confetti Blog</h1>
+    const composedPosts = this.state.posts.map((item, index) => {
+      return (
+        <li key={index} className='post'>
+          <h3 className='postTitles'>{item.title}</h3>
+          <p>{item.content}</p>
+          <h6>{item.user}</h6>
+        </li>
+      )
+    })
     return (
     <div className="App container">
       <Nav />
         {this.state.isShowing ? title : null}
+        <ul>{composedPosts}
+        </ul>
         <button onClick={this.handleClick}>Click meeeee!</button>
       <Footer />
     </div>
